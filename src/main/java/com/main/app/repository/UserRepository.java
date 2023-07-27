85a64ce45f54d2d22dffe63e2a28b9e056c3c137
K1000-O <cjeneconde@gmail.com>
2023-07-27 14:05:16 +0200

    minor changes

--- src/main/java/com/main/app/repository/UserRepository.java
+++ src/main/java/com/main/app/repository/UserRepository.java
@@ -7,13 +7,11 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import com.main.app.user.User;
 
 import jakarta.transaction.Transactional;
 
-@Repository
 @EnableJpaRepositories
 public interface UserRepository extends JpaRepository<User, Long> {
 
